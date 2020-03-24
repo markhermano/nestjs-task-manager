b8bb1994d48e3d645af84cd6eddb892107a58068
Mark <marking.dav@gmail.com>
2020-03-24 19:27:32 +0800

    tests

--- src/auth/user.respository.ts
+++ src/auth/user.respository.ts
@@ -10,7 +10,7 @@ export class UserRepository extends Repository<User> {
   async signUp(authSignupCredentialsDto: AuthSignUpCredentialsDto): Promise<void> {
     const { username, password } = authSignupCredentialsDto;
 
-    const user = new User();
+    const user = this.create();
     user.username = username;
     user.salt =  await bcrypt.genSalt();
     user.password = await this.hashPassword(password, user.salt);
